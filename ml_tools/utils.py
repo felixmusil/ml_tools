3a532e6925f90f9ef739dff665c7f01f2b8df726
musil <felix.musil@epfl.ch>
2018-07-13 14:47:45 +0200

    aa

--- ml_tools/utils.py
+++ ml_tools/utils.py
@@ -21,3 +21,54 @@ def make_new_dir(fn):
     if not os.path.exists(fn):
         os.makedirs(fn)
     return fn
+
+def is_notebook():
+    from IPython import get_ipython
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+if is_notebook():
+    from tqdm import tqdm_notebook as tqdm_cs
+    ascii = False
+else:
+    from tqdm import tqdm as tqdm_cs
+    ascii = True
+
+def s2hms(time):
+    m = time // 60
+    s = int(time % 60)
+    h = int(m // 60)
+    m = int(m % 60)
+    return '{:02d}:{:02d}:{:02d} (h:m:s)'.format(h,m,s)
+
+
+def qp2ase(qpatoms):
+    from ase import Atoms as aseAtoms
+    positions = qpatoms.get_positions()
+    cell = qpatoms.get_cell()
+    numbers = qpatoms.get_atomic_numbers()
+    pbc = qpatoms.get_pbc()
+    atoms = aseAtoms(numbers=numbers, cell=cell, positions=positions, pbc=pbc)
+
+    for key, item in qpatoms.arrays.iteritems():
+        if key in ['positions', 'numbers', 'species', 'map_shift', 'n_neighb']:
+            continue
+        atoms.set_array(key, item)
+
+    return atoms
+
+def ase2qp(aseatoms):
+    from quippy import Atoms as qpAtoms
+    positions = aseatoms.get_positions()
+    cell = aseatoms.get_cell()
+    numbers = aseatoms.get_atomic_numbers()
+    pbc = aseatoms.get_pbc()
+    return qpAtoms(numbers=numbers,cell=cell,positions=positions,pbc=pbc)
