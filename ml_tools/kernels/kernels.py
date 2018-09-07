62771395b81b5ab77304987810796e88e0ba811e
musil <felix.musil@epfl.ch>
2018-09-07 19:11:02 +0200

    add fps filter and SoR sparse kernel

--- ml_tools/kernels/kernels.py
+++ ml_tools/kernels/kernels.py
@@ -42,3 +42,44 @@ class KernelPower(KernelBase):
     def loads(self,state):
         self.zeta = state['zeta']
         self.delta = state['delta']
+
+
+
+class KernelSparseSoR(KernelBase):
+    def __init__(self,kernel,pseudo_input_ids=None):
+        self.delta = kernel.delta # \sim std of the training properties
+        self.kernel = kernel
+        self.pseudo_input_ids = pseudo_input_ids
+        
+    def fit(self,X):   
+        return self
+    def get_params(self,deep=True):
+        params = dict(kernel=self.kernel,pseudo_input_ids=self.pseudo_input_ids)
+        return params
+    def set_params(self,**params):
+        self.kernel = params['kernel']
+        self.delta = self.kernel.delta
+        self.pseudo_input_ids = params['pseudo_input_ids']
+        
+    def transform(self,X,y=None,X_train=None):
+        if X_train is None and isinstance(X,dict) is False and y is not None:
+            Xs = X[self.pseudo_input_ids]
+            kMM = self.kernel(Xs,Y=Xs)
+            kMN = self.kernel(Xs,Y=X)
+            ## assumes Lambda= delta**2*np.diag(np.ones(n))
+            sparseK = kMM + np.dot(kMN,kMN.T)/self.delta**2
+            sparseY = np.dot(kMN,y)/self.delta**2
+
+            return sparseK,sparseY
+        else: 
+            return self.kernel(X,Y=X_train)
+
+
+    def pack(self):
+        state = self.get_params()
+        return state
+    def unpack(self,state):
+        pass
+    
+    def loads(self,state):
+        self.set_params(state)
