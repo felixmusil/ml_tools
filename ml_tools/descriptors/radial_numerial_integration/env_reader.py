fba0ee4da7f263f9da544ccb2eccafc82e08d0c9
felixmusil <felix.musil@epfl.ch>
2019-01-22 21:02:49 +0100

    typo

--- ml_tools/descriptors/radial_numerial_integration/env_reader.py
+++ ml_tools/descriptors/radial_numerial_integration/env_reader.py
@@ -1,8 +1,9 @@
 
+from .ge import gaussian_expansion as ge
+
 import numpy as np
 from ase.io import read
 from ase.geometry import find_mic
-from .ge import gaussian_expansion as ge
 
 ##########################################################################################
 
