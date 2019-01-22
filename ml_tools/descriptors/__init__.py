b853e78b26788889c0f88fafaf80b383a4b46f5b
musil <felix.musil@epfl.ch>
2019-01-22 20:58:42 +0100

    typo

--- ml_tools/descriptors/__init__.py
+++ ml_tools/descriptors/__init__.py
@@ -1,8 +1,10 @@
+
+from .internal_interface import RawSoapInternal
+
 try:
     from .quippy_interface import RawSoapQUIP
     has_quippy = True
 except:
     has_quippy = False
 
-from .internal_interface import RawSoapInternal
 
