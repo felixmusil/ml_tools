322febaad196082d6985d294d56866948228c9f6
musil <felix.musil@epfl.ch>
2019-01-23 19:43:17 +0100

    rename folder and change setup file

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import sys
 from numpy.distutils.core import Extension, setup
 
 
-__author__ = "Anders S. Christensen"
-__copyright__ = "Copyright 2016"
-__credits__ = ["Anders S. Christensen et al. (2016) https://github.com/qmlcode/qml"]
+__author__ = "Félix Musil"
+__copyright__ = "Copyright 2019"
+__credits__ = [""]
 __license__ = "MIT"
-__version__ = "0.4.0.12"
-__maintainer__ = "Anders S. Christensen"
-__email__ = "andersbiceps@gmail.com"
+__version__ = "0.1"
+__maintainer__ = "Félix Musil"
+__email__ = "musil.felix@gmail.com"
 __status__ = "Beta"
-__description__ = "Quantum Machine Learning"
-__url__ = "https://github.com/qmlcode/qml"
+__description__ = "Tools to do machine learning with atomic configurations"
+__url__ = "https://github.com/felixmusil/ml_tools"
 
 
 FORTRAN = "f90"
@@ -28,9 +28,9 @@ if sys.platform == "darwin" and all(["gnu" not in arg for arg in sys.argv]):
     LINKER_FLAGS = []
 
 
-ext_ = Extension(name = 'ml_tools.descriptors.radial_numerial_integration.ge',
+ext_ = Extension(name = 'ml_tools.descriptors.approx_dirac_radial_basis.ge',
                 sources = [
-                      'ml_tools/descriptors/radial_numerial_integration//gaussian_expansion.f90',
+                      'ml_tools/descriptors/approx_dirac_radial_basis/gaussian_expansion.f90',
                   ],
                 extra_f90_compile_args = COMPILER_FLAGS,
                 extra_f77_compile_args = COMPILER_FLAGS,
@@ -48,7 +48,7 @@ def setup_pepytools():
 
         name="ml_tools",
         packages=['ml_tools','ml_tools.compressor','ml_tools.descriptors',
-                    'ml_tools.descriptors.radial_numerial_integration',
+                    'ml_tools.descriptors.approx_dirac_radial_basis',
                     'ml_tools.kernels','ml_tools.math_utils','ml_tools.model_selection',
                     'ml_tools.models','ml_tools.split','ml_tools.utils',
                     'ml_tools.hasher',
@@ -58,7 +58,7 @@ def setup_pepytools():
         version=__version__,
         author=__author__,
         author_email=__email__,
-        platforms = 'Any',
+        platforms = 'UNIX like',
         description = __description__,
         keywords = [],
         classifiers = [],
