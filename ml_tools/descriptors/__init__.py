a3d0fee2044564d36cdb3e19bcc7e4d90522f373
musil <felix.musil@epfl.ch>
2019-02-21 10:57:15 +0100

    change import try catch

--- ml_tools/descriptors/__init__.py
+++ ml_tools/descriptors/__init__.py
@@ -3,16 +3,14 @@ import warnings
 try:
     from .internal_interface import RawSoapInternal
     has_internal_soap = True
-except:
+except ImportError:
     warnings.warn("Could not import internal descriptor",category=ImportWarning)
     has_internal_soap = False
-
 
 try:
     from .quippy_interface import RawSoapQUIP
     has_quippy = True
-except:
+except ImportError:
     warnings.warn("Could not import quippy",category=ImportWarning)
     has_quippy = False
-
 
